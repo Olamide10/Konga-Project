584079fdf0d5ece88cbebee9ad7de7194ddc1789
Oladeji Olamide <olamidewunmi10@gmail.com>
2018-11-08 22:21:51 +0000

    QUIZ 3 INTRODUCTION TO PHP

---
+++ AssignmentPHP/QUIZ 3.php
@@ -0,0 +1,12 @@
+<?php
+$temperature = '20';
+if ($temperature <= 20){
+        echo "Its really cold today";
+}elseif  ($temperature > 20 && $temperature >30){
+    echo "The weather is just perfect";
+}elseif ($temperature >= 30 && $temperature <40){
+    echo "Its so hot today";
+}elseif ($temperature >= 40);{
+    echo "I'm I in sahara desert?";
+}
+?>
