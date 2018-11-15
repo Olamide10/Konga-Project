0ab4b60b72d6053a03ab4cb3783776122c618b2a
Olamide10 <43090797+Olamide10@users.noreply.github.com>
2018-11-15 23:11:13 +0000

    Update QUIZ 1.php

--- AssignmentPHP/QUIZ 1.php
+++ AssignmentPHP/QUIZ 1.php
@@ -1,6 +1,6 @@
 <?php
 // QUIZ 1 
 echo "Olamide "."Oladeji <br>";
-echo "is "."a "."young "."lady, "."passionate "."about "."self-development. <br>";
-echo "she"." has "."a "."degree "."in "."Architecture. <br><br>";
+echo "is a young lady, passionate about self-development. <br>";
+echo "she has a degree in Architecture. <br><br>" ;
 ?>
