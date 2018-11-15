3e1457f5781f5dde097f3fe5e3fbdb19c8da4436
Olamide10 <43090797+Olamide10@users.noreply.github.com>
2018-11-15 23:12:19 +0000

    Update QUIZ 2.php

--- AssignmentPHP/QUIZ 2.php
+++ AssignmentPHP/QUIZ 2.php
@@ -4,13 +4,13 @@ $J = '5';
 $K = '10';
 //Multiply the first and third variable and store in a variable 
 $x= $I*$K;
-echo nl2br("The product of I and K is ". $x );
+echo nl2br("The product of $I and $K is ". $x );
 
 //Subtract the first variable from the second and store in a variable
 $y= $J-$I;
-echo nl2br("The subtraction of I from J is".$y);
+echo nl2br("The subtraction of $I from $J is".$y);
 
 // Divide the third and the second variable and store in variable 
 $z= $K/$J;
-echo nl2br("The division of K and J is". $z );
+echo nl2br("The division of $K and $J is". $z );
 ?>
