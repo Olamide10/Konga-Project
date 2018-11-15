8ef0b34bf7dd0c1ace283d919db80a59bcb50431
Olamide10 <43090797+Olamide10@users.noreply.github.com>
2018-11-15 23:14:50 +0000

    Update QUIZ 5.php

--- AssignmentPHP/QUIZ 5.php
+++ AssignmentPHP/QUIZ 5.php
@@ -1,9 +1,9 @@
 <?php
 $firstname = array("olumide", "ifedayo", "ejiro" );
 $hobbies = array("drawing", "writing", "baking");
-$f_h = array_combine ($firstname, $hobbies);
 
-foreach($f_h as $x=>$x_value){
-     echo "My name is "."$x "." I love ". $x_value. ".<br>";
- } 
+
+for($i = 0; $i<count($firstnamename); $i++){
+    echo "My name is $firstname[$i]. I love $hobbies[$i]\n";
+}
 ?>
