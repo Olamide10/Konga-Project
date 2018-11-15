1ce6c164afe1188058692299b9801f13a265b292
Olamide10 <43090797+Olamide10@users.noreply.github.com>
2018-11-15 23:13:08 +0000

    Update QUIZ 3.php

--- AssignmentPHP/QUIZ 3.php
+++ AssignmentPHP/QUIZ 3.php
@@ -2,7 +2,7 @@
 $temperature = '20';
 if ($temperature <= 20){
         echo "Its really cold today";
-}elseif  ($temperature > 20 && $temperature >30){
+}elseif  ($temperature > 20 && $temperature <30){
     echo "The weather is just perfect";
 }elseif ($temperature >= 30 && $temperature <40){
     echo "Its so hot today";
