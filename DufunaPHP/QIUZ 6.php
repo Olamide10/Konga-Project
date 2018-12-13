56bba04ea4b002017443aef8b905e6671cf29ccc
Olamide10 <43090797+Olamide10@users.noreply.github.com>
2018-12-13 02:23:49 +0000

    Quiz 6

---
+++ DufunaPHP/QIUZ 6.php
@@ -0,0 +1,25 @@
+<?php
+
+$w=100;      	
+$u=50;
+
+function add() {     	
+		 
+$sum=$w+$u;
+         	
+echo "The sum of w and u is".$sum;
+}
+	
+function sub(){         	
+	
+$sub=$w-$u;
+         	
+echo "The subtraction of w from u is".$sub;
+}
+	  
+//call function whenever you need
+	  
+add();
+sub();
+
+?>
